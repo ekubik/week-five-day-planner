a36aef0908d77371b170432af4c1a7350f84469b
Ewa Kubik <ewa.kubik@hotmail.com>
2021-12-22 22:08:17 +1100

    first time block save function implemented

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 var currentDate = document.getElementById("currentDay");
 currentDate.textContent = moment().format("dddd, MMMM Do YYYY");
+
+var currentHour = moment().format("H");
+
+var saveButton = document.getElementById("button-9");
+var textArea9AM = document.getElementById("nine-am");
+var eventsNine = textArea9AM.value;
+
+saveButton.addEventListener("click", function () {
+  console.log(eventsNine);
+  var eventsNine = textArea9AM.value;
+  localStorage.setItem("hourlyEvent", eventsNine);
+  console.log(eventsNine);
+});
+
+function displayNineAm() {
+  textArea9AM.innerText = localStorage.getItem("hourlyEvent");
+}
+
+displayNineAm();
